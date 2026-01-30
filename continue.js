b51abe3a028cb85dff6e693f5f074fdae4514ec0
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2026-01-31 03:48:19 +0600

    continue or (skip)

---
+++ continue.js
@@ -0,0 +1,22 @@
+//? continue = skip (it's work like skip);
+
+//* Task - 1 Solution:
+//  TODO: Write a loop to print even numbers from 1 to 40. Use continue to skip odd numbers.
+
+for (let i = 1; i <= 40; i++) {
+    if (i % 2 === 1) {
+        continue
+    }
+    console.log(i);
+}
+
+/* ------------------------------ Task Divider ------------------------------ */
+//* Task - 2 Solution:
+//  TODO: display odd number from 55 to 85 and skip the numbers divisible by 5.
+
+for (let i = 55; i <= 85; i++) {
+    if (i % 5 === 0) {
+        continue
+    }
+    console.log(i);
+}
